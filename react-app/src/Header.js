084d30853f81e89114c29b8751424798586c0d0c
Sergei Anikeev <anikeevsu@gmail.com>
2022-04-27 19:16:53 +1200

    organizing files

---
+++ react-app/src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default function Header(){
+    return  (
+    <header className="App-header">
+      <nav className="nav">
+        <img className='nav-logo' src={logo}/>
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+      </nav>
+    </header>
+      )
+    }
+    
+ 
