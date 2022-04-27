620a152eac2151398c158fc083a9ddc4ac3f3575
Sergei Anikeev <anikeevsu@gmail.com>
2022-04-27 20:39:19 +1200

    add components and images

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import './style.css';
 import Header from './Header';
 import Footer from './Footer';
 import MainContent from './MainContent';
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
-function App() {
+export default function App() {
   const page = (
   <div className="App">
-    <Header />
-    <MainContent/>
+    <Navbar />
+    <Main/>
     <Footer/>
+    
   </div>
   
   )
   return page;
 }
-
-export default App;
