c4c0fc342d0bf04634141a91d272518ebc8d3b59
Sergei Anikeev <anikeevsu@gmail.com>
2022-04-29 10:52:53 +1200

    update Main

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './images/logo.svg';
 import './style.css';
 import Header from './Header';
-import Footer from './Footer';
+import Footer from './components/Footer';
 import MainContent from './MainContent';
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
